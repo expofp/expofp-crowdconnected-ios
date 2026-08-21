3d31fec501964683d50f7429640ea7f525f707d2
anatolii.frolov <af2905g@gmail.com>
2026-08-21 20:26:47 +0300

    Release v5.4.1

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@
 # Set bash script to exit immediately if any commands fail
 set -e
 
-VERSION="5.4.0"
+VERSION="5.4.1"
 PACKAGE_NAME="ExpoFpCrowdConnected"
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
